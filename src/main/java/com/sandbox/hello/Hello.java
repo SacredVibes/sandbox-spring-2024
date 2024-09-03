7979b99b24b2f0dc143cb2a5829f54b954d4167f
Manuel Martinez <man2153618@maricopa.edu>
2024-09-02 20:33:27 -0700

    Adding hello World

---
+++ src/main/java/com/sandbox/hello/Hello.java
@@ -0,0 +1,11 @@
+package com.sandbox.hello;
+
+import javax.sound.midi.SysexMessage;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
